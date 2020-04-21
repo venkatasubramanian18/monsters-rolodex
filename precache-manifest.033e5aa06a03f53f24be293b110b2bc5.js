02ca5b8ab9c5e1825d2d4eadc538cbb946c04433
Venkatasubramanian Subburaman <venkatasubramanian18@gmail.com>
2020-04-21 11:35:18 +0530

    Updates

---
+++ precache-manifest.033e5aa06a03f53f24be293b110b2bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28f8fa02c2b4775eb9b44fd27c22785",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e6732bbaa01e78f57f23",
+    "url": "/monsters-rolodex/static/css/main.72d12306.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6732bbaa01e78f57f23",
+    "url": "/monsters-rolodex/static/js/main.f7127dd5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
